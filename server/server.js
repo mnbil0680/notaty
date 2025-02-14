b2f6ab2f92189136d972563d2623c96b56e1a3ee
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-02-15 00:42:36 +0200

    update mongoose to version 8.10.1 and modify database connection URL

--- server/server.js
+++ server/server.js
@@ -5,24 +5,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const Database = require('./Database');
 const db = new Database();
-app.use(cors()); // it is very important to make API calls from different domain
-app.use(bodyParser.json()); // parse the incoming request body
-app.use(bodyParser.urlencoded({ extended: false })); // parse the incoming request body
+
+app.use(cors()); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
-
-// Most common API routes
-/**
- * POST - Create   ==> Create a new note
- * GET - Read      ==> Get all notes information
- * GET - Read      ==> Get information of a specific note
- * PUT - Update    ==> Update a note
- * DELETE - Delete ==> Delete a note
- */
-
-// Temporary data store even can be used as a database
-let notes = [];
-
+let notes = []; //empty array to store notes
 // Create POST API to be able to create a new note
 app.post('/notes', (req, res) =>{
     const body = req.body;
@@ -30,8 +19,8 @@ app.post('/notes', (req, res) =>{
     notes.push(body.title);
     console.log("NOTES:", notes);
     res.send(true);
-    //res.status(201).send('Note is created');
-}); // ("/notes", callBack function(req, res) => { });
+    
+}); 
 
 // create GET API to be able to get all notes
 app.get('/notes', (req, res) => {
