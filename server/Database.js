66e09457fbcdc9e3273989afa84c6d706bc7bf0f
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-02-14 23:28:36 +0200

    update database connection URL format and improve logging messages BUT still there is an  error

--- server/Database.js
+++ server/Database.js
@@ -1,22 +1,15 @@
 const mongoose = require('mongoose'); // Import mongoose library to be able to connect to the database
-
-
 class Database{
     constructor(){
-        this.UrlUrl = 'mongodb://localhost:27017/notaty'; // URL to connect to the database through path and name of the database
-
+        this.Url = 'mongodb://0.0.0.0:27017/notaty'; // URL to connect to the database through path and name of the database
     }
-        // remove connect option useNewUrlParser has no effect since Node.js Driver version 4.0.0
         connect(){
-            mongoose.connect(this.UrlUrl, {}).then(()=>{
-                console.log('Databasee Connected Successfully');
+            mongoose.connect(this.Url, {}).then(()=>{
+                console.log("Database Connected Successfully");
             }).catch((err)=>{
-                console.log('Error in Database Connection', err);
+                console.log("Error in Database Connection", err);
             }) // Connect to the database
         }
-
-    
 }
-
 
 module.exports = Database; // Export the Database class to be able to use it in other files
