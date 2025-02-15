63fb3f3e1cc4db3d9654f08704477ebfd1e81f8f
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-02-15 12:02:36 +0200

    stable version

--- server/Database.js
+++ server/Database.js
@@ -2,12 +2,10 @@ const mongoose = require('mongoose'); // Import mongoose library to be able to c
 class Database{
 
     constructor(){
-        this.Url = 'mongodb://localhost:7000/notaty'; // Database URL
+        this.Url = 'mongodb://localhost:27017/notaty'; // Database URL
         }
         connect(){
-            mongoose.connect(this.Url, {
-                useNewUrlParser: true,
-                useUnifiedTopology: true}).then(()=>{
+            mongoose.connect(this.Url, {}).then(()=>{
                 console.log("Database Connected Successfully");
             }).catch((err)=>{
                 console.log("Error in Database Connection", err);
