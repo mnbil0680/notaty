a9547bb35a5700e9ec7d70f18756aff427f35ab1
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-02-16 04:58:54 +0200

    Add edit and delete functionality to notes table with corresponding icons

--- client/note-handler.js
+++ client/note-handler.js
@@ -10,10 +10,34 @@ function updateNotesTable(){
             let cell2 = row.insertCell(1);
             let cell3 = row.insertCell(2);
             let cell4 = row.insertCell(3);
-
+            // adding style to the cell4
+            cell4.style.display = "flex";
+            cell4.style.justifyContent = "space-between";
+            
             cell1.innerHTML = note["title"];
             cell2.innerHTML = note["content"];
             cell3.innerHTML = note["updatedDate"];
+            // adding two images for edit and delete
+            cell4.innerHTML =
+            `<a href="#"
+            style= "text-decoration: none;
+            margin: auto;">
+                <img 
+                src="./images/edit.png" alt="edit"
+                style="width: 22px; height: 22px;"
+                onclick="editNote(${note["id"]})">
+            </a>
+            <a href="#"
+            style= "text-decoration: none;
+            margin: auto;">
+                <img
+                src="./images/delete.png" alt="delete"
+                style="width: 22px; height: 22px;"
+                onclick="deleteNote(${note["id"]})">
+            </a>
+            `
+
+            
 
         });
     });
